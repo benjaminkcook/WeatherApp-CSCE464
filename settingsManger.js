81f5e16554b346c30fb4ae1d51eabb1fdf0a96f1
Benjamin Cook <90069170+benjaminkcook@users.noreply.github.com>
2024-03-17 23:03:34 -0500

    Update settingsManger.js

--- settingsManger.js
+++ settingsManger.js
@@ -7,11 +7,32 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function applyDarkModeStyles(darkMode) {
+    const navbarLinks = document.querySelectorAll('.navbar a');
+
     if (darkMode) {
         document.body.style.backgroundColor = '#333333';
         document.body.style.color = '#F3EEEA';
+        navbarLinks.forEach(link => {
+            link.style.color = '#F3EEEA';
+            link.addEventListener('mouseover', function() {
+                link.style.color = '#B0A695';
+            });
+            link.addEventListener('mouseout', function() {
+                link.style.color = '#F3EEEA';
+            });
+        });
+        
     } else {
         document.body.style.backgroundColor = '#F3EEEA';
         document.body.style.color = '#000000';
+        navbarLinks.forEach(link => {
+            link.style.color = '#000000';
+            link.addEventListener('mouseover', function() {
+                link.style.color = '#B0A695';
+            });
+            link.addEventListener('mouseout', function() {
+                link.style.color = '#000000';
+            });
+        });
     }
 }
